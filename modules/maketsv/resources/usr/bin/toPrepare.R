7a423ca34f9d359e1df9c8e95e3c9bf8b40953c9
giuseppemartone <119871332+giuseppemartone@users.noreply.github.com>
2025-02-10 10:45:47 +0100

    Update toPrepare.R

--- modules/maketsv/resources/usr/bin/toPrepare.R
+++ modules/maketsv/resources/usr/bin/toPrepare.R
@@ -79,6 +79,7 @@ OverrideCycles <- gsub(",", ";", OverrideCycles)
     resuMa1[12,1]<-paste0("CreateFastqForIndexReads,1")
     }
  resuMa1[13,1]<-"BarcodeMismatchesIndex1=0"
+ resuMa1[14,1]<-"BarcodeMismatchesIndex1=0"
 
   resuMa2<-matrix(ncol=ncols,nrow=length(sst[,1])+2)
   resuMa2[1,1]<-"[Data]"
